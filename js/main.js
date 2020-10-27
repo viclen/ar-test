79a7762f2dc3b94dda3226b79bbf3388887bf749
Victor <victorpbi5@hotmail.com>
2020-10-27 16:50:08 -0300

    fix: test smoothing function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-AFRAME.registerComponent('gallery', {
+function getLocation(cb = () => { }) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(cb);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    const objects = document.getElementById("objects");
+
+    objects.setAttribute("gps-entity-place", "latitude: " + position.coords.latitude + "; longitude: " + position.coords.longitude);
+    objects.setAttribute("visible", "true");
+}
+
+AFRAME.registerComponent('ar-scene', {
     init: function () {
         const clickToStart = document.getElementById('clickToStart');
 
         clickToStart.addEventListener('click', () => {
             const screen = document.getElementById('screen');
 
-            screen.setAttribute('position', '0.2 1.6 -2.7');
+            screen.setAttribute('position', '0 0 -10');
             screen.setAttribute('visible', 'true');
-            screen.setAttribute('scale', '0.8 0.8 0.8');
+            screen.setAttribute('scale', '1 1 1');
+
+            getLocation(showPosition)
 
             clickToStart.remove();
         });
@@ -36,5 +53,41 @@ AFRAME.registerComponent('gallery', {
         image6.addEventListener('click', function (e) {
             window.open("https://www.sadia.com.br/");
         })
+    }
+})
+
+AFRAME.registerComponent("listener", {
+    init: function () {
+        this.target = document.querySelector('#target'); // your video
+        this.prevPosition = null; // initially there is no position or rotation
+        this.prevRotation = null;
+    },
+
+    tick: function () {
+        if (this.el.object3D.visible) {
+            this.target.setAttribute('visible', 'true')
+
+            if (!this.prevPosition && !this.prevRotation) {
+                // there are no values to lerp from - set the initial values
+                this.target.setAttribute('position', this.el.getAttribute('position'))
+                this.target.setAttribute('rotation', this.el.getAttribute('rotation'))
+            } else {
+                // use the previous values to get an approximation 
+                this.target.object3D.position.lerp(this.prevPosition, 0.1)
+
+                // this (below) may seem ugly, but the rotation is a euler, not a THREE.Vector3, 
+                // so to use the lerp function i'm doing some probably unnecessary conversions
+                let rot = this.target.object3D.rotation.toVector3().lerp(this.prevRotation, 0.1)
+                this.target.object3D.rotation.setFromVector3(rot)
+            }
+            // update the values
+            this.prevPosition = this.el.object3D.position
+            this.prevRotation = this.el.object3D.rotation
+        } else {
+            // the marker dissapeared - reset the values
+            this.target.setAttribute('visible', 'false')
+            this.prevPosition = null;
+            this.prevRotation = null;
+        }
     }
 })

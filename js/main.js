32f0651ca3c24cf6431c509bdd93fff99673a446
Victor <victorpbi5@hotmail.com>
2020-10-27 19:21:38 -0300

    fix: moved href to links

--- js/main.js
+++ js/main.js
@@ -15,24 +15,17 @@ function showPosition(position) {
 
 let isOpening = false;
 function openurl(url) {
-    if (!isOpening) {
-        window.open(url);
-        isOpening = true;
-        setTimeout(() => isOpening = false, 200);
-    }
+    window.open(url);
 
     console.log(url);
 
     return isOpening;
 }
 
-AFRAME.registerComponent('log-hover', {
+AFRAME.registerComponent('ar-link', {
     init: function () {
-        this.el.addEventListener('mouseenter', function () {
-            console.log('mouseenter', this.id);
-        });
-        this.el.addEventListener('mouseleave', function () {
-            console.log('mouseleave', this.id);
+        this.el.addEventListener('click', function () {
+            openurl(this.getAttribute('href'));
         });
     }
 });
@@ -52,27 +45,5 @@ AFRAME.registerComponent('ar-scene', {
 
             clickToStart.remove();
         });
-
-        document.getElementById('index1').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/quantomaisvocesabe/receitas/?ingredientes=linguiças");
-        })
-        document.getElementById('index2').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/quantomaisvocesabe/receitas/?ingredientes=peito%20de%20peru");
-        })
-        document.getElementById('index3').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/bio/");
-        })
-        document.getElementById('index4').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/quantomaisvocesabe/receitas/?ingredientes=frango");
-        })
-        document.getElementById('index5').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/quantomaisvocesabe/menu-da-semana/semana-1/");
-        })
-        document.getElementById('index6').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/");
-        })
-        document.getElementById('logo-sadia-index').addEventListener('click', function (e) {
-            openurl("https://www.sadia.com.br/");
-        })
     }
 });

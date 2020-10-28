e348c6cfb7395e5b398772c949561bea8525458b
Victor <victorpbi5@hotmail.com>
2020-10-28 15:33:22 -0300

    fix: testing raycaster

--- js/main.js
+++ js/main.js
@@ -99,9 +99,30 @@ AFRAME.registerComponent('log-intersection', {
     }
 });
 
-document.addEventListener("click", function () {
-    if (initialized && lastItem) {
-        console.log('click', lastItem.getAttribute("src"));
-        openurl(lastItem.getAttribute('href'));
+AFRAME.registerComponent('raycaster-listen', {
+    init: function () {
+        // Use events to figure out what raycaster is listening so we don't have to
+        // hardcode the raycaster.
+        this.el.addEventListener('raycaster-intersected', evt => {
+            this.raycaster = evt.detail.el;
+        });
+        this.el.addEventListener('raycaster-intersected-cleared', evt => {
+            this.raycaster = null;
+        });
+    },
+
+    tick: function () {
+        if (!this.raycaster) { return; }  // Not intersecting.
+
+        let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+        if (!intersection) { return; }
+        console.log(intersection.point);
     }
 });
+
+// document.addEventListener("click", function () {
+//     if (initialized && lastItem) {
+//         console.log('click', lastItem.getAttribute("src"));
+//         openurl(lastItem.getAttribute('href'));
+//     }
+// });
